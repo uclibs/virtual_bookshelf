bec8fcb66781f60d62abab12df1303c82f1d0970
Avinash Nukala <nvsk.avinash@yahoo.com>
2020-12-15 17:55:33 -0500

    clickable images with comments

--- api3.py
+++ api3.py
@@ -10,44 +10,86 @@ from IPython.display import HTML
 from io import BytesIO
 import struct
 
+"""
+There are two steps for getting a response using sierra API
+        1) Authenticating with the Patron API
+        2) Making a Sierra API call
+"""
+#Step1
+#Loading Client key and secret key from the file
 f=open('C:\\uc\\uclib\\credentials.txt', 'r')
 lines = f.readlines()
 
 user=lines[0].replace('\n','')
 pwd=lines[1].replace('\n','')
+
+#Combining the API key and secret into a single string separated by a colon:
 comb = user+':'+pwd
+#Converting the combined key:secret string into a Base64 string
 comb_enc = base64.b64encode(bytes(comb, 'utf-8')).decode("utf-8") 
 
+#Setting the Header name field to authorization.
+#Setting the Header value field to Basic, and inserting the Base64-encoded credentials
 headers = {
-    #'authorization': "Basic "+(user+":"+pwd).base64.b64encode("base64"),
-    #'authorization': "Basic "+base64.b64encode(bytes((user+":"+pwd),encoding='utf8')),
-    #'authorization': "Basic NGRpRERaVzVlK2pFdjNLNE1yaFNLeGorNmUyejpwM21hOHBAVw==",
     'authorization': "Basic "+comb_enc,
     'content-type': "application/json",
 }
 
-r = requests.post('https://uclid.uc.edu:443/iii/sierra-api/v5/token', headers=headers)
-data = r.json()
-print(r)
-print(data)
+#Set the method to POST
+r_step1 = requests.post('https://uclid.uc.edu:443/iii/sierra-api/v5/token', headers=headers)
+data = r_step1.json()
 
+#Here we will store the access_token in a variable named myToken
 myToken = data['access_token']
-print(myToken)
 
+#Step2
+#We will use the captured access token to make our API call
+#Setting the Header value field to Bearer, and inserting the access token
 headers1 = {
     'authorization': 'Bearer '+str(myToken), 
     'content-type': "application/json",
 }
 
-r1 = requests.get('https://uclid.uc.edu:443/iii/sierra-api/v5/bibs/search?fields=id%2Ctitle%2Cauthor%2CvarFields&text=PS3616.U%3F%3F%3F%3F', headers=headers1)
-json_string = r1.json()
+#Set the method to GET and capture the json response into a variable
+r_step2 = requests.get('https://uclid.uc.edu:443/iii/sierra-api/v5/bibs/search?fields=id%2Ctitle%2Cauthor%2CvarFields&text=PS3616.U%3F%3F%3F%3F', headers=headers1)
+json_string = r_step2.json()
+
+"""
+    Creating a hashmap named syndetics for our application where:
+        key : (bib id, title of book, authors of book)
+        value :  [Array of subfields parameter of the json response where fieldTag of a particular book is 'i']
+        
+    After creating an empty hash map we will loop through the json response obtained to fill the hashmap
+"""
 syndetics=defaultdict(list)
+
+#Loading data into the syndetics hashmap
 for i in range(len(json_string['entries'])):
     for j in range(len(json_string['entries'][i]['bib']['varFields'])):
         if(json_string['entries'][i]['bib']['varFields'][j]['fieldTag']=='i'):
             for k in range(len(json_string['entries'][i]['bib']['varFields'][j]['subfields'])):
                 syndetics[(json_string['entries'][i]['bib']['id'],json_string['entries'][i]['bib']['title'],json_string['entries'][i]['bib']['author'])].append(json_string['entries'][i]['bib']['varFields'][j]['subfields'][k]['content'])
 
+"""
+    Here in the following code we will loop through value array of each key.
+    Since ISBN numbers are stored in the value array, we will loop through them
+    If we were able to get image for atleast one ISBN number in the array while traversing through the value array
+    We will break there and store the [url, corresponding bib number] from which we are getting the image into an array named image_array
+    
+    If we had completed traversing the value array till end but still if we are unable to find the image for not even a single ISBN 
+    number present in the value array that means there is no image existed in our database.
+    In such case we are generating a manual image with title and author on the book cover.
+    This condition is checked using a variable named count.
+    If count is equal to the length of the value array which means we have looped through the entire value array
+    but haven't found image for any of the ISBN's then we will enter into the second if statement.
+    
+    I have used a module named pillow in python for generating manual images.
+    I have used a package named textwrap which fits the title and author names within the bounds of the book cover
+    
+    If we have manually generated an image, then the manually generated image is stored in a temporary location locally 
+    and the [path, corresponding bib number] is appended into the image_array
+    
+"""
 image_array=[]
 for key,value in syndetics.items():
     count=0
@@ -57,17 +99,11 @@ for key,value in syndetics.items():
         if(bytes('large cover image', 'utf-8') in response.content):
             count+=1
         else:
-            # print("yes cover")
             image_bytes = io.BytesIO(response.content)
             img = PIL.Image.open(image_bytes)
-            #img.show()
             image_array.append(['http://www.syndetics.com/index.php?isbn='+str(value[i])+'/lc.jpg',key[0]])
-            #image_array.append(image_bytes)
-            #image_array.append(img)
             break
         if(count==len(value)):
-            # print("No cover Module")
-            # print("No cover")
             W=200
             H=300
             msg="\n"+"Title: "+str(key[1])+"\n"+"Author: "+str(key[2])
@@ -80,78 +116,31 @@ for key,value in syndetics.items():
                 w, h = d.textsize(line, font=fnt)
                 d.text(((W - w) / 2, current_h), line, font=fnt)
                 current_h += h + pad
-            # w, h = d.textsize(para)
-            # ((W-w)/2,(H-h)/2)
-            # d.text((250,120), msg , font=fnt, fill=(255, 255, 0))
-            #img.show()
             arb="./"
             path=arb+str(json_string['entries'][i]['bib']['id'])+".png"
             img.save(path) 
             image_array.append([path,key[0]])
             
-# from IPython.display import Image as img_ipy     
-# def _src_from_data(data):
-    #str = base64.b64encode(imageFile.read())
-#     data_uri = base64.b64encode(Image.fromarray(data)).decode('utf-8')
-#     img_tag = '<img src="data:image/png;base64,{0}">'.format(data_uri)
-#     return img_tag
-#     """Base64 encodes image bytes for inclusion in an HTML img element"""
-#     data=data.tobytes()
-#     data_uri = base64.b64encode(Image.fromarray(data)).decode('utf-8')
-#     img_tag = '<img src="data:image/png;base64,{0}">'.format(data_uri)
-#     return img_tag
-    #print(data)
-    #print("\n")
-    #print("**************************************************")
-#     data=data.tobytes()
-#     data_uri = data.read().encode('base64').replace('\n', '')
-#     return f'<img src="data:image/png;base64,{0}" width="400" height="275">'.format(data_uri)
-#     #print("\n")
-#     #print(data)
-#     def convert_string_to_bytes(string):
-#         bytes = b''
-#         for i in string:
-#             bytes += struct.pack("B", ord(i))
-#         return bytes
-#     stream = BytesIO(convert_string_to_bytes(data))
-#     image = Image.open(stream).convert("RGBA")
-#     stream.close()
-# #     image.show()
-#     img_obj = img_ipy(data=data)
-#     for bundle in img_obj._repr_mimebundle_():
-#         for mimetype, b64value in bundle.items():
-#             #if mimetype.startswith('image/'):
-#             return f'data:{mimetype};base64,{b64value}'
-#     data_uri = base64.b64encode(open(str(data), 'rb').read()).decode('utf-8')
-#     img_tag = '<img src="data:image/png;base64,{0}">'.format(data_uri)
-#     return img_tag
             
 def gallery(images, row_height='150px'):
-    """Shows a set of images in a gallery that flexes with the width of the notebook.
+    """Shows a set of images of the books that sits next to each other within the width of the notebook.
     
     Parameters
     ----------
-    images: list of str or bytes
-        URLs or bytes of images to display
+    images: list of 
+        URLs or path where the manual image is stored
 
     row_height: str
         CSS height value to assign to all images. Set to 'auto' by default to show images
-        with their native dimensions. Set to a value like '250px' to make all rows
+        with their native dimensions. Set to a value like '150px' to make all rows
         in the gallery equal height.
+       
+    returning the HTML template
     """
     figures = []
     for image1 in images:
-        #image1.show()
-        #print(type(image1))
         reference="http://uclid.uc.edu.proxy.libraries.uc.edu/record=b"+str(image1[1])+"~S39"
-        if '.png' in image1[0]:
-            #print("entered if")
-            src = image1[0]
-            #print(image1[1])
-            #print(src)
-        else:
-            #print("entered else")
-            src = image1[0]
+        src = image1[0]
         figures.append(f'''
             <figure style="margin: 5px !important;">
               <a href = '{reference}'>
